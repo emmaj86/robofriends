d7497b44a37e87c6e276aa5149147c04da3965ce
emma jamieson <emma.cody@hotmail.co.uk>
2023-02-21 11:36:43 +0000

    change to app.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <h1>Hello world</h1>
-    <app />
+    <App />
   </React.StrictMode>
 );
 
